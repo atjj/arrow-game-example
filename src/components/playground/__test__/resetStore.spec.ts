25730369df7bf9a94f165d1887a724ca7a0a4de2
Atay <atajmamasadykov@gmail.com>
2026-02-03 15:32:48 +0600

    add redux toolkit reducer test

---
+++ src/components/playground/__test__/resetStore.spec.ts
@@ -0,0 +1,16 @@
+import playgroundReducer, {
+  initialState,
+  setCurrentStep,
+  resetStore,
+} from "../slices"
+
+describe("reducer resetStore", () => {
+  it("check  resetStore", () => {
+    const setCurrentStepState = playgroundReducer(
+      initialState,
+      setCurrentStep(),
+    )
+    const resetStoreState = playgroundReducer(setCurrentStepState, resetStore())
+    expect(resetStoreState).toEqual(initialState)
+  })
+})
